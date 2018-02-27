0f2cbad97789baa2e271d409cc78adddcecef3bb
Zack Stout <zackstout@gmail.com>
2018-02-26 21:42:45 -0600

    it seems like there must be a way to avoid all these loops

--- draw.js
+++ draw.js
@@ -4,16 +4,46 @@ var w = 800;
 var h = 800;
 var cellWid, cellHeight;
 var num = 30;
+var start = 0;
+var visitedCells = [];
+
+function carvePassage(cell) {
+  // console.log(cell);
+  var neighbors = getNeighbors(cell);
+  console.log(neighbors);
+  var realNeighbors = [];
+  // check whether the neighbors have been visited:
+  neighbors.forEach(function(n) {
+    if (!visitedCells.includes(n)) {
+      realNeighbors.push(n);
+    }
+  });
+  var rand = Math.floor(Math.random() * realNeighbors.length);
+  var next = realNeighbors[rand];
+  console.log(next);
+  //
+  // var nextReal = cells.filter(function(c) {
+  //   return c.id == next;
+  // });
+  //
+  // console.log(nextReal);
+  //
+  // nextReal.visited = true;
+  // return next;
+}
 
 function setup() {
   createCanvas(w, h);
   drawGrid();
   console.log(cells);
-  cells.forEach(function(cell, index) {
-    if (index < 50) {
-      console.log(getNeighbors(cell), cell.id);
-    }
+  // ahhh, returns an array:
+  var startCell = cells.filter(function(c) {
+    return c.id == start;
   });
+  // console.log(startCell);
+  visitedCells.push(start);
+  carvePassage(startCell[0]);
+
 }
 
 function draw() {
@@ -23,23 +53,28 @@ function draw() {
 
 function getNeighbors(cell) {
   var up, down, left, right;
+  var output = [];
   // check if on top border:
   if (cell.id % num != 0) {
     up = cell.id - 1;
+    output.push(up);
   }
   // check if on bottom border:
   if (cell.id % num != (num - 1)) {
     down = cell.id + 1;
+    output.push(down);
   }
   // check if on left border:
   if (cell.id >= num) {
     left = cell.id - num;
+    output.push(left);
   }
   // check if on right border:
   if (cell.id <= w - cellWid) {
     right = cell.id + num;
+    output.push(right);
   }
-  var output = [up, right, down, left];
+
   return output;
 }
 
@@ -53,7 +88,7 @@ function drawGrid() {
       var cell = {
         x: i * cellWid,
         y: j * cellHeight,
-        visited: false,
+        // visited: false,
         id: i * num + j,
       };
       cells.push(cell);
