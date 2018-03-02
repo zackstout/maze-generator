94509e2ffaa0087b8c1f28112f1387b86f9526b5
Zack Stout <zackstout@gmail.com>
2018-03-01 20:40:59 -0600

    excellent, backtracking is basically working. we can get up to a path of 500 without an error

--- draw.js
+++ draw.js
@@ -11,12 +11,15 @@ var cellTwo;
 var numCells = 0;
 
 var path = [];
+var deadEnd = false;
+
+var nextCell, currentCell;
 
 // Nuts, it passes arrays by reference......:
-var test = [1, 2];
-var test2 = test;
-test2.pop();
-console.log(test2, test);
+// var test = [1, 2];
+// var test2 = test;
+// test2.pop();
+// console.log(test2, test);
 
 // Problem: you'd think you could just save the previous cell as the one to backtrack to, because it must have had open neighbors. Issue is that its last neighbor might have been the deadend. How do we get back to the previous cell with live neighbors?
 // Can we say that the prev cell has 2 possibilities? Because if only has one, it's heading for the dead end.
@@ -25,94 +28,97 @@ console.log(test2, test);
 
 
 function carvePassage(cell) {
-  cellTwo = {};
-  // console.log(cell);
-  var neighbors = getNeighbors(cell);
-  // console.log(neighbors);
-  var realNeighbors = [];
-  // check whether the neighbors have been visited:
-  neighbors.forEach(function(n) {
-    if (!visitedCells.includes(n)) {
-      realNeighbors.push(n);
-    }
-  });
-
-  // Need to backtrack if no unvisited neighbors:
-  if (realNeighbors.length == 0) {
-    console.log('uh oh!');
-    console.log(path);
-    console.log(cellTwo);
-
-
-
-    // while (path.length > 0) {
-    //   var prev = path.pop();
-    //   var neigh = getNeighbors(prev);
-    //   var neighReal = [];
-    //   neigh.forEach(function(x) {
-    //     if (!visitedCells.includes(x)) {
-    //       neighReal.push(x);
-    //     }
-    //   });
-    //   console.log(neighReal);
-    //   if (neighReal.length !== 0) {
-    //     carvePassage(neighReal);
-    //     // return;
-    //   }
-    // }
-
-  // There is an available (i.e. unvisited) adjacent cell:
+  // console.log(numCells);
+  if (numCells > 500) {
+    console.log(('all done!'));
+    return;
   } else {
+    var neighbors = getNeighbors(cell);
+    var realNeighbors = [];
 
-    // Get next cell:
-    var rand = Math.floor(Math.random() * realNeighbors.length);
-    var next = realNeighbors[rand];
-    visitedCells.push(next);
-    var nextReal = cells.filter(function(c) {
-      return c.id == next;
-    });
+    if (!deadEnd) {
+      // Check whether the neighbors have been visited:
+      neighbors.forEach(function(n) {
+        if (!visitedCells.includes(n)) {
+          realNeighbors.push(n);
+        }
+      });
 
-    var currentReal = cells.filter(function(c) {
-      return c.id == cell.id;
-    });
-    console.log(currentReal[0]);
+      // Need to backtrack if no unvisited neighbors:
+      if (realNeighbors.length == 0) {
+        console.log('uh oh!');
+        console.log(path);
+        deadEnd = true;
+        // console.log(deadEnd);
+        carvePassage(currentCell);
 
-    path.push(nextReal[0]);
-    numCells ++;
+      // There is an available (i.e. unvisited) adjacent cell:
+      } else {
 
-    // console.log(cell);
+        // Get next cell:
+        var rand = Math.floor(Math.random() * realNeighbors.length);
+        var next = realNeighbors[rand];
+        visitedCells.push(next);
+        var nextReal = cells.filter(function(c) {
+          return c.id == next;
+        });
 
+        var currentReal = cells.filter(function(c) {
+          return c.id == cell.id;
+        });
+        // last one of these logged is the one *before* the dead end:
+        console.log(currentReal[0]);
+        currentCell = currentReal[0];
 
-    // This is very odd, why is cellTwo empty here?:
-    console.log(cell.id, realNeighbors, nextReal[0], cellTwo);
-    if (realNeighbors.length > 1) {
-      cellTwo = currentReal[0];
-      console.log(cellTwo);
-      // console.log(cellTwo);
+        // last element of failed path is the dead end cell itself:
+        path.push(nextReal[0]);
+        numCells ++;
+
+        // Remove barrier:
+        stroke(41, 100, 200);
+        strokeWeight(3);
+        if (nextReal[0].id == cell.id + 1) {
+          line(cell.x, cell.y + cellHeight, cell.x + cellWid, cell.y + cellHeight);
+        } else if (nextReal[0].id == cell.id - 1) {
+          line(cell.x, cell.y, cell.x + cellWid, cell.y);
+        } else if (nextReal[0].id == cell.id + num) {
+          line(cell.x + cellWid, cell.y, cell.x + cellWid, cell.y + cellHeight);
+        } else if (nextReal[0].id == cell.id - num) {
+          line(cell.x, cell.y, cell.x, cell.y + cellHeight);
+        }
+        nextCell = nextReal[0];
+        carvePassage(nextReal[0]);
+      } // end ELSE statement
+
+    // we hit a dead end:
+    } else {
+      console.log('dead end ');
+      // get rid of dead end:
+      path.pop();
+      while (deadEnd) {
+        var last = path.pop();
+        // check last's neighbors:
+        var neigh = getNeighbors(last);
+        var realNeigh = [];
+        neigh.forEach(function(neighb) {
+          if (!visitedCells.includes(neighb)) {
+            realNeigh.push(neighb);
+          }
+        });
+        if (realNeigh.length == 0) {
+          // We are still dead, need to keep backtracking:
+        } else {
+          deadEnd = false;
+          carvePassage(last);
+        }
+      }
+      // carvePassage(currentCell);
+
     }
-    console.log(cell.id, realNeighbors, nextReal[0], cellTwo);
+    // Recurse:
 
 
-    // console.log(cell.id, nextReal[0].id);
-
-    // Remove barrier:
-    stroke(41, 100, 200);
-    strokeWeight(3);
-
-    if (nextReal[0].id == cell.id + 1) {
-      line(cell.x, cell.y + cellHeight, cell.x + cellWid, cell.y + cellHeight);
-    } else if (nextReal[0].id == cell.id - 1) {
-      line(cell.x, cell.y, cell.x + cellWid, cell.y);
-    } else if (nextReal[0].id == cell.id + num) {
-      line(cell.x + cellWid, cell.y, cell.x + cellWid, cell.y + cellHeight);
-    } else if (nextReal[0].id == cell.id - num) {
-      line(cell.x, cell.y, cell.x, cell.y + cellHeight);
-    }
-
-
-    carvePassage(nextReal[0]);
-  } // end ELSE statement
-
+  }
 
 }
 
